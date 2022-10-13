e43b4c887f635ddc99fcc7b33878a9fe972965c7
lucas720px <lucas1080px@gmail.com>
2022-10-13 17:34:53 -0300

    atualization banner primary radios

--- script.js
+++
@@ -1,9 +0,0 @@
-const btnMobile = document.getElementById("header__btn-mobile")
-
-function toggleMenu() {
-    const nav = document.getElementsByClassName("header__child-navbar");
-    nav.classList.toggle("active");
-}
-
-btnMobile.addEventListener("click", toggleMenu);
-btnMobile.onclick
